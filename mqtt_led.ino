9ea6d636b6731dc3a445b766c99e298778a501ab
sesiliafenina <fenina.gunawan2605@gmail.com>
2021-08-15 21:28:30 +0800

    removed uneeded lines

--- mqtt_led.ino
+++ mqtt_led.ino
@@ -1,16 +1,3 @@
-// NeoPixelTest
-// This example will cycle between showing four pixels as Red, Green, Blue, White
-// and then showing those pixels as Black.
-//
-// Included but commented out are examples of configuring a NeoPixelBus for
-// different color order including an extra white channel, different data speeds, and
-// for Esp8266 different methods to send the data.
-// NOTE: You will need to make sure to pick the one for your platform 
-//
-//
-// There is serial output of the current state so you can confirm and follow along
-//
-
 #include <NeoPixelBrightnessBus.h>
 #include "WiFi.h"
 #include <PubSubClient.h>
